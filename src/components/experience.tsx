60ea7785a728d58d959e119ded847aaee8789dcb
Hemashree KJ <hema@ominaya.com>
2026-03-13 11:42:51 +0530

    project

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import { Briefcase } from 'lucide-react';
 import { siteConfig } from '@/data/siteConfig';
+import { cn } from '@/lib/utils';
 
 export const Experience = () => {
   return (
